158b64a41965b66ca725b5a1a4596324621b9f10
hz21046493 <HcP6k5h3I3>
2021-11-03 18:29:10 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main()  {
+	r :=gin.Default()
+	// 测试路由
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK,"pong")
+	})
+	
+	// 启动服务
+	err := r.Run(":8080")
+	if err != nil {
+		return 
+	}
+}
