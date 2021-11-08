73a2de4781718c88cf02dd6873d7313619b39eb6
hz21046493 <HcP6k5h3I3>
2021-11-08 16:58:34 +0800

    7

--- app/common/request/user.go
+++ app/common/request/user.go
@@ -3,7 +3,7 @@ package request
 type Register struct {
 	Name     string `form:"name" json:"name" binding:"required"`
 	Mobile   string `form:"mobile" json:"mobile" binding:"required,mobile"`
-	Password string `form,:"password" json:"password" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
 }
 
 // GetMessages 自定义错误信息
@@ -12,6 +12,19 @@ func (register Register) GetMessages() ValidatorMessages {
 		"name.required":     "用户名称不能为空",
 		"mobile.required":   "手机号码不能为空",
 		"mobile.mobile":     "手机号码格式不正确",
+		"password.required": "用户密码不能为空",
+	}
+}
+
+type Login struct {
+	Mobile   string `form:"mobile" json:"mobile" binding:"required,mobile"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+func (login Login) GetMessages() ValidatorMessages {
+	return ValidatorMessages{
+		"mobile.required":   "手机号码不能为空",
+		"password.mobile":   "手机号码格式不正确",
 		"password.required": "用户密码不能为空",
 	}
 }
--- routes/api.go
+++ routes/api.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"jassue-gin/app/common/request"
 	"jassue-gin/app/controllers/app"
+	"jassue-gin/app/middleware"
+	"jassue-gin/app/services"
 	"net/http"
 	"time"
 )
@@ -33,4 +35,12 @@ func SetApiGroupRoutes(router *gin.RouterGroup) {
 	})
 
 	router.POST("/auth/register", app.Register)
+
+	router.POST("/auth/login", app.Login)
+
+	authRouter := router.Group("").Use(middleware.JWTAuth(services.AppGuardName))
+	{
+		authRouter.POST("/auth/info", app.Info)
+	}
+
 }
