6b6692f2df3186c1765b1e069dd09f4d0f5ed17f
hz21046493 <HcP6k5h3I3>
2021-11-08 18:37:26 +0800

    8

--- config/jwt.go
+++ config/jwt.go
@@ -1,6 +1,7 @@
 package config
 
 type Jwt struct {
-	Secret string `mapstructure:"secret" json:"secret" yaml:"secret"`
-	JwtTtl int64  `mapstructure:"jwt_ttl" json:"jwt_ttl" yaml:"jwt_ttl"`
+	Secret                  string `mapstructure:"secret" json:"secret" yaml:"secret"`
+	JwtTtl                  int64  `mapstructure:"jwt_ttl" json:"jwt_ttl" yaml:"jwt_ttl"`
+	JwtBlacklistGracePeriod int64  `mapstructure:"jwt_blacklist_grace_period" json:"jwt_blacklist_grace_period" yaml:"jwt_blacklist_grace_period"` // 黑名单宽限时间（秒）
 }
